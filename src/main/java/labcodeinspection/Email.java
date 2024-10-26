705c69fed5c737e9fd4a2d3cccf9b88a3649104f
Kevin-QQ-82 <kquintun@espol.edu.ec>
2024-10-25 23:28:30 -0500

    correcciones de la parte 2

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,51 +1,82 @@
 package labcodeinspection;
 
-public class Email {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
-	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
+@SuppressWarnings("PMD.UseUtilityClass")
 
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
-	}
+/**
+*
+*
+* @author Kevin
+*/
 
-	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
-	}
+public class Email { //NOPMD class comments are too long to explain this class
+    private static final Logger log = Logger.getLogger(Email.class.getName());//NOPMD its already log
 
-	public void setDeparment(int depChoice) {
-		switch (depChoice) {
-		case 1:
-			this.department = "sales";
-			break;
-		case 2:
-			this.department = "dev";
-			break;
-		case 3:
-			this.department = "acct";
-			break;
-		}
-	}
+    private final String m_firstName; //NOPMD its already manipulated
+    private final String m_lastName;  //NOPMD its already manipulated
+    private String password = ""; 	//NOPMD its already manipulated
+    private String department;		//NOPMD its already manipulated
+    private final int defaultPasswordLength = 8; //NOPMD its already manipulated
+    private String email; //NOPMD its already manipulated
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
-		char[] password = new char[length];
-		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
-			password[i] = set.charAt(rand);
-		}
-		return new String(password);
-	}
+    /**
+     * Constructor to initialize first name and last name.
+     * 
+     * @param firstName the first name of the user
+     * @param lastName  the last name of the user
+     */
+    public Email(final String firstName, final String lastName) {
+        this.m_firstName = firstName;
+        this.m_lastName = lastName;
+    }
 
-	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
-				+ ".espol.edu.ec";
-	}
+    /**
+     * Displays the information of the email.
+     */
+    public void showInfo() {
+        if (log.isLoggable(Level.INFO)) {
+            log.info(String.format("\nFIRST NAME= %s\nLAST NAME= %s", m_firstName, m_lastName));
+            log.info(String.format("DEPARTMENT= %s\nEMAIL= %s\nPASSWORD= %s", department, email, password));
+        }
+
+    /**
+     * Sets the department based on user choice.
+     * 
+     * @param depChoice the choice of department
+     */
+    public void setDepartment(int depChoice) {  //NOPMD THIS METOD WILL BE USED IN THE FUTURE
+        switch (depChoice) {
+            case 1:S
+                this.department = "sales";
+                break;
+            case 2:
+                this.department = "dev";
+                break;
+            case 3:
+                this.department = "acct";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid department choice.");
+        }
+    }
+
+    /**
+     * Generates a random password of the specified length.
+     * 
+     * @param length the length of the password
+     * @return a random password
+     */
+    private String randomPassword(final int length) { //NOPMD THIS METOD WILL BE USED IN THE FUTURE
+        final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+        final char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            final int rand = (int) (Math.random() * set.length());
+            password[i] = set.charAt(rand);
+        }
+        return new String(password);
+    }
+
 }
+
--- src/main/java/labcodeinspection/EmailApp.java
+++ src/main/java/labcodeinspection/EmailApp.java
@@ -1,26 +1,25 @@
 package labcodeinspection;
-
 import java.util.Scanner;
-
+@SuppressWarnings("PMD.UseUtilityClass")
+/**
+ * Main application class for handling email operations.
+ */
 public class EmailApp {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
-
-		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
-
-		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
-
-		int depChoice = sc.nextInt();
-		sc.close();
-
-		Email email = new Email(firstName, lastName);
-		email.setDeparment(depChoice);
-		email.generateEmail();
-		email.showInfo();
-	}
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        try {
+            final System.out.print("Enter your first name: ");
+            String firstName = sc.nextLine();
+            System.out.print("Enter your last name: ");
+            final String lastName = sc.nextLine();
+            
+            final Email email = new Email(firstName, lastName);
+            email.setDepartment(1); // Ejemplo de uso
+            email.showInfo();
+        } finally {
+            sc.close(); // Asegura que el Scanner se cierre
+        }
+    }
 }
+
